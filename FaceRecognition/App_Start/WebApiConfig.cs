59380f015712293050315abbb48dbc09c4a453fd
Vladislav Smirnov <vdmsmirnov@gmail.com>
2017-06-11 19:28:16 +0300

    Fix some staff, at least I think so

--- FaceRecognition/App_Start/WebApiConfig.cs
+++ FaceRecognition/App_Start/WebApiConfig.cs
@@ -15,7 +15,7 @@ namespace FaceRecognition
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "Api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
